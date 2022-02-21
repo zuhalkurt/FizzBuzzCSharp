7cd12fb8b2d9176a521a1a072c5d81f95af85ae7
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-02-21 13:16:49 +0000

    Division by seven and eleven added

--- FizzBuzzer.cs
+++ FizzBuzzer.cs
@@ -2,12 +2,18 @@ namespace FizzBuzz {
     public class FizzBuzzer {
         public string FizzBuzz(int number){
             string output = "";
-            if(number % 3 == 0){
+            if(number % 3 == 0 && number % 11 != 0){
                     output += "Fizz";
                 }
-            if(number % 5 == 0){
+            if(number % 5 == 0 && number % 11 != 0){
                     output += "Buzz";
                 }
+            if(number % 7 == 0 && number % 5 != 0 && number % 11 != 0) {
+                output += "Bang";
+            }
+            if(number % 11 == 0){
+                output += "Bong";
+            }
             if(output == ""){
                 return number.ToString();
             }
